6c0c739210fff117e945f1dfec02f34fee8fed3c
DiCerso <lautaro_cer@hotmail.com>
2022-03-14 13:06:36 -0300

    importacion de home.html

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const app = express();
 
 const path = require('path');
 const port = 3030;
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    return res.sendFile(path.resolve(__dirname,'views','home.html'))
+});
+
+
+app.listen(port, () => console.log("pagina corriendo en puerto http://localhost:3030"))
