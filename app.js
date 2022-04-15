0417806cbc8c5cd53545712c94c4e716170062fc
DiCerso <lautaro_cer@hotmail.com>
2022-04-14 21:14:00 -0300

    formulario de login y register

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ const port = 3030;
 app.use(express.static('public'))
 
 app.get('/', (req,res) => {
-    return res.sendFile(path.resolve(__dirname,'views','home.html'))
+    return res.sendFile(path.resolve(__dirname,'views','index.html'))
 });
 
+app.get('/register', (req,res) => {
+    return res.sendFile(path.resolve(__dirname,'views','register.html'))
+});
+
+app.get('/login', (req,res) => {
+    return res.sendFile(path.resolve(__dirname,'views','login.html'))
+});
 
 app.listen(port, () => console.log("pagina corriendo en puerto http://localhost:3030"))
